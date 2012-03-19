7f29c91b9a1627f26fd2d7e48a1c4060b321705f
Randall Mason <randall@mason.ch>
2012-03-19 09:51:58 +0000

    drop lines without colon

--- bgwiktionaryParse.sh
+++ bgwiktionaryParse.sh
@@ -8,4 +8,4 @@ test -n "$(find . -maxdepth 1 -name 'bgwiktionary-*-pages-meta-current.xml' -pri
     cat bgwiktionary-latest-pages-meta-current.xml  | grep -e "^<mediawiki xmlns" -e "<title>" -e "<text" | grep -B1 "#виж" | grep -v -- "^--$" | sed -e 's/<text xml:space="preserve">.*#виж \[\[\(.*\)\]\].*/<text xml:space="preserve">#виж [[\1]]/g' | sed -e 's/<title>/<page><title>/g' -e 's#</text>#</text></page>#g' -e 's#\]\]$#\]\]</text></page>#g' | grep -B1 "#виж \[" | grep -v -- "^--$"
     tail -1 bgwiktionary-latest-pages-meta-current.xml
     echo ""
-) | tidy -xml -utf8 | grep -e title -e \#виж | grep -v : | grep -A1 title | vim -c ":%s#</title>\n##g" -c ":%s/#виж \[\[/:/g" -c ":%s/.*<title>//" -c ":%s/\]\]//g" -c ":%s/^:.*//g" -c ":%s/^$\n//g" -c ":wq! pairs.txt" -
+) | tidy -xml -utf8 | grep -e title -e \#виж | grep -v : | grep -A1 title | vim -c ":%s#</title>\n##g" -c ":%s/#виж \[\[/:/g" -c ":%s/.*<title>//" -c ":%s/\]\]//g" -c ":%s/^[^:]\+$//g" -c ":%s/^:.*//g" -c ":%s/\n^$//g" -c ":wq! pairs.txt" -
