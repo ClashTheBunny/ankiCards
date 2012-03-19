12fd6f67c70632815b61082ceea53dec6625b933
Randall Mason <randall@mason.ch>
2012-03-19 08:04:56 +0000

    updates from home

--- bgwiktionaryParse.sh
+++ bgwiktionaryParse.sh
@@ -1,1 +1,9 @@
-(echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"; head -1 bgwiktionary-20111106-pages-meta-current.xml; cat bgwiktionary-20111106-pages-meta-current.xml  | grep -e "^<mediawiki xmlns" -e "<title>" -e "<text" | grep -B1 "#виж" | grep -v -- "^--$" | sed -e 's/<text xml:space="preserve">.*#виж \[\[\(.*\)\]\].*/<text xml:space="preserve">#виж [[\1]]/g' | sed -e 's/<title>/<page><title>/g' -e 's#</text>#</text></page>#g' -e 's#\]\]$#\]\]</text></page>#g'; tail -1 bgwiktionary-20111106-pages-meta-current.xml; echo "";) | tidy -xml -utf8 > pairs.xml
+#!/bin/bash
+
+(
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
+    head -1 bgwiktionary-20111116-pages-meta-current.xml
+    cat bgwiktionary-20111116-pages-meta-current.xml  | grep -e "^<mediawiki xmlns" -e "<title>" -e "<text" | grep -B1 "#виж" | grep -v -- "^--$" | sed -e 's/<text xml:space="preserve">.*#виж \[\[\(.*\)\]\].*/<text xml:space="preserve">#виж [[\1]]/g' | sed -e 's/<title>/<page><title>/g' -e 's#</text>#</text></page>#g' -e 's#\]\]$#\]\]</text></page>#g'
+    tail -1 bgwiktionary-20111116-pages-meta-current.xml
+    echo ""
+) | tidy -xml -utf8 | grep -e title -e \#виж | grep -v : | grep -A1 title | vim -c ":%s#</title>\n##g" -c ":%s/#виж \[\[/:/g" -c ":%s/.*<title>//" -c ":%s/\]\]//g" -c ":%s/^:.*//g" -c ":%s/^$\n//g" -c ":wq! pairs.txt" -
--- wordList.py
+++ wordList.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import codecs
-import string
+#import string
 import re
 import bgdict
 import csv
 
-(bg_en,en_bg) = bgdict.buildDicts()
+(bg_en, en_bg, bg_bg, ) = bgdict.buildDicts()
 
 dirname = ''
 filename = 'Jordan_Jovkov_-_Zhetvarjat-7865.txt'
@@ -14,20 +14,32 @@ f = codecs.open(dirname + filename, 'r', 'utf-8')
 
 text = f.read()
 
-wordBoundry = re.compile('\W+',re.UNICODE)
+def makeFileFromText(text, filename):
+    wordBoundry = re.compile('\W+',re.UNICODE)
 
-freqDict = {}
-for w in wordBoundry.split(text):
-	w = w.lower()
-	if freqDict.has_key(w):
-		freqDict[w] += 1
-	else:
-		freqDict[w] = 1
+    freqDict = {}
+    for w in wordBoundry.split(text):
+        w = w.lower()
+        if freqDict.has_key(w):
+            freqDict[w] += 1
+        else:
+            freqDict[w] = 1
 
-csvWriter = csv.writer(open(filename + '.csv', 'wb'))
+    csvWriter = csv.writer(open(filename + '.csv', 'wb'))
 
-for tup in sorted(sorted(freqDict.items(), key=lambda x: x[0].encode('utf-8')), key=lambda x: x[1], reverse=True):
-	try:
-		csvWriter.writerow([tup[0].encode('utf-8') , bg_en[tup[0]].encode('utf-8')] )
-	except:
-		print tup[0].encode('utf-8')
+    for tup in sorted(sorted(freqDict.items(), key=lambda x: x[0].encode('utf-8')), key=lambda x: x[1], reverse=True):
+        try:
+            csvWriter.writerow([tup[0].encode('utf-8') , bg_en[bg_bg[tup[0]]].encode('utf-8')] )
+        except:
+            try:
+                csvWriter.writerow([tup[0].encode('utf-8') , bg_en[tup[0]].encode('utf-8')] )
+            except:
+                print tup[0].encode('utf-8')
+
+chapBoundry = re.compile(u'Глава \d+',re.UNICODE)
+
+for chapter in chapBoundry.split(text):
+    print "chapter"
+    print chapter.encode('utf-8')
+    print "end chapter"
+    makeFileFromText(chapter, filename )
