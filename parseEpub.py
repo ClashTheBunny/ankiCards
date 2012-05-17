d1499e7ad672c9296749219e949c9d300a50e2c2
Randall Mason <ClashTheBunny@gmail.com>
2012-05-17 20:03:46 +0300

    functionalize parseText and parseEpub so I can use them from elsewhere

--- parseEpub.py
+++ parseEpub.py
@@ -22,35 +22,39 @@ if debug:
         from IPython import embed
         ipshell = embed
 
-filename = sys.argv[1]
+if __name__ == '__main__':
+    for filename in sys.argv[1:]:
+        epub2csv(filename)
 
-if not os.path.isabs( filename ):
-    filename = os.path.abspath( filename )
+def epub2csv(filename):
 
-epub = zipfile.ZipFile(filename)
-
-metaDom = xml.dom.minidom.parseString(epub.open("META-INF/container.xml").read())
-
-opsFile = metaDom.getElementsByTagName("rootfile")[0].getAttributeNode("full-path").value
-
-opsDom = xml.dom.minidom.parseString(epub.open(opsFile).read())
-
-section = 0
-
-allWords = []
-
-for chapter in opsDom.getElementsByTagName("spine")[0].getElementsByTagName("itemref"):
-    section = section + 1
-    for element in opsDom.getElementsByTagName("manifest")[0].getElementsByTagName("item"):
-        if element.getAttribute("id") == chapter.getAttributeNode("idref").value:
-            chapterFilename = element.getAttribute("href")
-    chapterText = epub.open(os.path.join(os.path.dirname(opsFile),chapterFilename)).read()
-    soup = BeautifulSoup.BeautifulSoup(chapterText)
-    body_text = ''.join(soup.body(text=True))
-    freqency = wordList.makeFreqFromText(body_text,allWords)
-    #print allWords
-    #print freqency.keys()
-    allWords = list(set(list(chain.from_iterable([ allWords, freqency.keys()]))))
-    # pprint(allWords)
-    wordList.createChapterFile(filename + ".cards/{:02d} - ".format(section) + chapterFilename + '.csv', freqency)
-    ankiImport.import_csv(filename + ".cards/{:02d} - ".format(section) + chapterFilename + '.csv', "BG", os.path.basename(filename), "{:02d}".format(section) + chapterFilename)
+    if not os.path.isabs( filename ):
+        filename = os.path.abspath( filename )
+    
+    epub = zipfile.ZipFile(filename)
+    
+    metaDom = xml.dom.minidom.parseString(epub.open("META-INF/container.xml").read())
+    
+    opsFile = metaDom.getElementsByTagName("rootfile")[0].getAttributeNode("full-path").value
+    
+    opsDom = xml.dom.minidom.parseString(epub.open(opsFile).read())
+    
+    section = 0
+    
+    allWords = []
+    
+    for chapter in opsDom.getElementsByTagName("spine")[0].getElementsByTagName("itemref"):
+        section = section + 1
+        for element in opsDom.getElementsByTagName("manifest")[0].getElementsByTagName("item"):
+            if element.getAttribute("id") == chapter.getAttributeNode("idref").value:
+                chapterFilename = element.getAttribute("href")
+        chapterText = epub.open(os.path.join(os.path.dirname(opsFile),chapterFilename)).read()
+        soup = BeautifulSoup.BeautifulSoup(chapterText)
+        body_text = ''.join(soup.body(text=True))
+        freqency = wordList.makeFreqFromText(body_text,allWords)
+        #print allWords
+        #print freqency.keys()
+        allWords = list(set(list(chain.from_iterable([ allWords, freqency.keys()]))))
+        # pprint(allWords)
+        wordList.createChapterFile(filename + ".cards/{:02d} - ".format(section) + chapterFilename + '.csv', freqency)
+        ankiImport.import_csv(filename + ".cards/{:02d} - ".format(section) + chapterFilename + '.csv', "BG", os.path.basename(filename), "{:02d}".format(section) + chapterFilename)
--- parseText.py
+++ parseText.py
@@ -10,22 +10,24 @@ import sys
 import ankiImport
 import os
 
-filename = sys.argv[1]
-
-if not os.path.isabs( filename ):
-    filename = os.path.abspath( filename )
-
-f = codecs.open(filename, 'r', 'utf-8')
-
-text = f.read()
-
-chapBoundry = re.compile(u'Глава (\d+)',re.UNICODE)
-
-allWords = ['',]
-
-for chapter in zip(chapBoundry.split(text)[1::2], chapBoundry.split(text)[2::2]):
-    freqency = wordList.makeFreqFromText(chapter[1],allWords)
-    # TODO: Fix capitals for names
-    allWords = list(set(list(chain.from_iterable([ allWords, freqency.keys()]))))
-    wordList.createChapterFile(filename + "{:02d}.csv".format(int(chapter[0])), freqency)
-    ankiImport.import_csv(filename + "{:02d}.csv".format(int(chapter[0])),"BG", os.path.basename(filename), "{:02d}".format(int(chapter[0])))
+def txt2csv(filename):
+    if not os.path.isabs( filename ):
+        filename = os.path.abspath( filename )
+    
+    f = codecs.open(filename, 'r', 'utf-8')
+    
+    text = f.read()
+    
+    chapBoundry = re.compile(u'Глава (\d+)',re.UNICODE)
+    
+    allWords = ['',]
+    
+    for chapter in zip(chapBoundry.split(text)[1::2], chapBoundry.split(text)[2::2]):
+        freqency = wordList.makeFreqFromText(chapter[1],allWords)
+        # TODO: Fix capitals for names
+        allWords = list(set(list(chain.from_iterable([ allWords, freqency.keys()]))))
+        wordList.createChapterFile(filename + "{:02d}.csv".format(int(chapter[0])), freqency)
+        ankiImport.import_csv(filename + "{:02d}.csv".format(int(chapter[0])),"BG", os.path.basename(filename), "{:02d}".format(int(chapter[0])))
+if __name__ == '__main__':
+    for filename in sys.argv[1:]:
+        txt2csv(filename)
