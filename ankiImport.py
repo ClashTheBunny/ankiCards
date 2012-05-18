52dc937ffca7796a98da07557577c4fd7c02711f
Randall Mason <randall@mason.ch>
2012-05-18 15:12:55 +0300

    Remove ankiqt from git modules and ankiImport.  It will be imported via being a plugin.

--- ankiImport.py
+++ ankiImport.py
@@ -5,7 +5,6 @@
 
 import os, sys
 sys.path.append(os.path.join(os.path.dirname( os.path.realpath( __file__ )),"libanki") )
-sys.path.append(os.path.join(os.path.dirname( os.path.realpath( __file__ )),"ankiqt") )
 
 from anki.importing import TextImporter
 import tempfile, os
